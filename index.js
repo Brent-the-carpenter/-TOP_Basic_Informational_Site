a7495d714f5ce230bf1d5bc071afcd5ca32bd557
Brent-the-carpenter <beh337@gmail.com>
2024-05-19 13:43:32 -0400

    I have got the refactored branch using express to work. I have also added a .gitignore file to ignore the node_modules directory.

--- index.js
+++ index.js
@@ -1,71 +1,81 @@
 import HTTP from "http";
 import FS from "fs/promises";
 import path, { extname } from "path";
-import url from "url";
+import url, { fileURLToPath } from "url";
+import express from "express";
 
-const readFile = async (file_name) => {
-  try {
-    const content = await FS.readFile(file_name, "utf-8");
-    return content;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 8080;
-const Server = () =>
-  HTTP.createServer(async (req, res) => {
-    const q = url.parse(req.url);
-    let file_name;
-    if (req.url === "/" || req.url === "/index.html") {
-      file_name = "./index";
-    } else {
-      console.log("Request url :", req.url);
-      file_name = "./public" + q.pathname;
-    }
-    console.log(req.url);
-    const Extension = extname(file_name);
-    if (!Extension) {
-      file_name += ".html";
-    }
-    let ContentType;
-    switch (Extension) {
-      case ".js":
-        ContentType = "text/javascript";
-        break;
 
-      case ".css":
-        ContentType = "text/css";
-        break;
-      case ".json":
-        ContentType = "application/json";
-        break;
-      case ".png":
-        ContentType = "image/png";
-        break;
-      case ".jpeg":
-        ContentType = "image/jpeg";
-        break;
-      default:
-        ContentType = "text/html";
-    }
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
+app.listen(PORT, () => console.log(`Server running on ${PORT}!`));
+// const readFile = async (file_name) => {
+//   try {
+//     const content = await FS.readFile(file_name, "utf-8");
+//     return content;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// };
 
-    try {
-      const content = await readFile(file_name);
-      res.writeHead(200, { "Content-Type": ContentType });
-      res.write(content);
-      console.log("Server is running on port:", PORT);
-      return res.end();
-    } catch (error) {
-      const not_found = await FS.readFile("./public/404.html");
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.write(not_found);
-      console.log("File not found!");
-      return res.end();
-    }
-  }).listen(PORT, () => {
-    console.log("Server is running on ", PORT);
-  });
+// const Server = () =>
+//   HTTP.createServer(async (req, res) => {
+//     const q = url.parse(req.url);
+//     let file_name;
+//     if (req.url === "/" || req.url === "/index.html") {
+//       file_name = "./index";
+//     } else {
+//       console.log("Request url :", req.url);
+//       file_name = "./public" + q.pathname;
+//     }
+//     console.log(req.url);
+//     const Extension = extname(file_name);
+//     if (!Extension) {
+//       file_name += ".html";
+//     }
+//     let ContentType;
+//     switch (Extension) {
+//       case ".js":
+//         ContentType = "text/javascript";
+//         break;
 
-Server();
+//       case ".css":
+//         ContentType = "text/css";
+//         break;
+//       case ".json":
+//         ContentType = "application/json";
+//         break;
+//       case ".png":
+//         ContentType = "image/png";
+//         break;
+//       case ".jpeg":
+//         ContentType = "image/jpeg";
+//         break;
+//       default:
+//         ContentType = "text/html";
+//     }
+
+//     try {
+//       const content = await readFile(file_name);
+//       res.writeHead(200, { "Content-Type": ContentType });
+//       res.write(content);
+//       console.log("Server is running on port:", PORT);
+//       return res.end();
+//     } catch (error) {
+//       const not_found = await FS.readFile("./public/404.html");
+//       res.writeHead(404, { "Content-Type": "text/html" });
+//       res.write(not_found);
+//       console.log("File not found!");
+//       return res.end();
+//     }
+//   }).listen(PORT, () => {
+//     console.log("Server is running on ", PORT);
+//   });
+
+// Server();
